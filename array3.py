e79db8f06d219a7e160cb8bbcc6b1a9be1a6664f
pyaswanth2805-lang <p.yaswanth2805@gmail.com>
2026-07-17 22:33:45 +0530

    Create array3.py

---
+++ array3.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5, 6])
+
+reshaped = arr.reshape(2, 3)
+
+print("Original Array:")
+print(arr)
+
+print("\nReshaped Array (2x3):")
+print(reshaped)
